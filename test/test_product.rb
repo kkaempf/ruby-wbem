878b60117c38d973a37511c2afdc404ce0cc2426
Klaus Kämpf <kkaempf@suse.de>
2012-06-28 13:45:13 +0200

    Re-enable connection tests for product

--- test/test_product.rb
+++ test/test_product.rb
@@ -8,15 +8,15 @@ class TestProduct < Test::Unit::TestCase
   end
   #def teardown
   #end
-  def xtest_product_port_5988
-    # 5988 -> http, cimxml
-    c = Wbem::Client.connect("http://wsman:secret@localhost:5988")
+  def test_product_port_5988
+    # 5989 -> https, cimxml
+    c = Wbem::Client.connect("https://wsman:secret@localhost:5989")
     assert c
     assert c.is_a? Wbem::CimxmlClient
     assert c.product
     puts "#{c.url} : #{c.product}"
   end
-  def xtest_product_port_5985
+  def test_product_port_5985
     # 5985 -> http, wman
     c = Wbem::Client.connect("http://wsman:secret@localhost:5985")
     assert c
@@ -25,27 +25,27 @@ class TestProduct < Test::Unit::TestCase
     puts "#{c.url} : #{c.product}"
   end
   def test_product_protocol_cimxml
-    c = Wbem::Client.connect("http://wsman:secret@localhost", :cimxml)
+    c = Wbem::Client.connect("https://wsman:secret@localhost", :cimxml)
     assert c
     assert c.is_a? Wbem::CimxmlClient
     assert c.product
     puts "#{c.url} : #{c.product}"
   end
-  def xtest_product_protocol_wsman
+  def test_product_protocol_wsman
     c = Wbem::Client.connect("http://wsman:secret@localhost", :wsman)
     assert c
     assert c.is_a? Wbem::WsmanClient
     assert c.product
     puts "#{c.url} : #{c.product}"
   end
-  def xtest_product_protocol_http_cimxml
-    c = Wbem::Client.connect("http://wsman:secret@localhost:5988", :cimxml)
+  def test_product_protocol_http_cimxml
+    c = Wbem::Client.connect("https://wsman:secret@localhost:5989", :cimxml)
     assert c
     assert c.is_a? Wbem::CimxmlClient
     assert c.product
     puts "#{c.url} : #{c.product}"
   end
-  def xtest_product_protocol_http_wsman
+  def test_product_protocol_http_wsman
     c = Wbem::Client.connect("http://wsman:secret@localhost:5985", :wsman)
     assert c
     assert c.is_a? Wbem::WsmanClient
